b8c5786e6f4f20ff9c1d9da30efa1cb847e9cde7
absencee_ <84275165+b1lodHand@users.noreply.github.com>
2024-04-30 10:41:04 +0300

    rework

--- Runtime/ShowIfAttribute.cs
+++ Runtime/ShowIfAttribute.cs
@@ -1,19 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+
 
 namespace com.absence.attributes
 {
-    public class ShowIfAttribute : HideIfAttribute
+    public class ShowIfAttribute : BaseIfAttribute
     {
         public ShowIfAttribute(string comparedPropertyName) : base(comparedPropertyName)
         {
-            Invert = true;
+            this.outputMethod = OutputMethod.ShowHide;
+            this.invert = true;
         }
 
         public ShowIfAttribute(string comparedPropertyName, object targetValue) : base(comparedPropertyName, targetValue)
         {
-            Invert = true;
+            this.outputMethod = OutputMethod.ShowHide;
+            this.invert = true;
         }
     }
 }
