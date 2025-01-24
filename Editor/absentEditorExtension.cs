b7400a53ad302370b4cb2baae0f4c3fee392b4b6
absencee_ <ilyasofli06@gmail.com>
2025-01-24 17:40:37 +0300

    Update absentEditorExtension.cs

--- Editor/absentEditorExtension.cs
+++ Editor/absentEditorExtension.cs
@@ -259,6 +259,8 @@ namespace com.absence.attributes.editor
 
             DrawFullInlineEditor();
 
+            return;
+
             void DrawFieldWithNewButton(int buttonId)
             {
                 EditorGUILayout.BeginHorizontal();
